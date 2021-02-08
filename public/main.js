87a04160ef0468b84966c0e02065edf197c60143
Russell Morgan <russell.morgan@gmail.com>
2021-02-08 14:06:31 -0500

    Made details page alpine enabled

---
+++ public/main.js
@@ -0,0 +1,21 @@
+// Get deatils section show appropriate data
+var section = location.hash;
+
+function details() {
+  return {
+    carousel:false,
+    title:'Wellness & Recreation',
+    intro:"<p>Morgan Stanley is committed to supporting and enhancing the physical and emotional health and wellbeing of our employees and their families. We promote wellness thru various programs like mediation, yoga, mindfulness session, etc</p><p>The Mother's Room facility is available at the Indiabulls, Athena, NKP, Wework and at Ecoworld. The room is designed to afford a private lactation space for nursing mothers and resting space for expectant mothers. </p>",
+    thumbs: [
+      "img/wellness/9930608-wellrec-ss-1440x960-1.jpg",
+      "img/wellness/9930608-wellrec-ss-1440x960-2.jpg",
+      "img/wellness/9930608-wellrec-ss-1440x960-3.jpg",
+      "img/wellness/9930608-wellrec-ss-1440x960-4.jpg",
+      "img/wellness/9930608-wellrec-ss-1440x960-5.jpg",
+      "img/wellness/9930608-wellrec-ss-1440x960-6.jpg",
+      "img/wellness/9930608-wellrec-ss-1440x960-7.jpg",
+      "img/wellness/9930608-wellrec-ss-1440x960-8.jpg",
+      "img/wellness/9930608-wellrec-ss-1440x960-9.jpg"
+    ]
+  }
+}
