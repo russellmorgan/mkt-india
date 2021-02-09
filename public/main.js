a509a49faa3afc28327523027aba336a9eb7cc8d
Russell Morgan <russell.morgan@gmail.com>
2021-02-09 16:35:00 -0500

    Setting up the hash logic for the data

--- public/main.js
+++ public/main.js
@@ -3,6 +3,11 @@ window.deferLoadingAlpine = function(callback) { document.addEventListener('DOMC
 
 // Get deatils section show appropriate data
 var section = location.hash;
+section = section.slice(1);
+console.log(section)
+
+// This is where we will set up the various strings based on the hash
+var title = "Wellness & Recreation";
 
 function details() {
   return {
@@ -24,7 +29,7 @@ function details() {
     },
     showCarousel:false,
     activeSlide: 0,
-    title:'Wellness & Recreation',
+    title:title,
     intro:"<p>Morgan Stanley is committed to supporting and enhancing the physical and emotional health and wellbeing of our employees and their families. We promote wellness thru various programs like mediation, yoga, mindfulness session, etc</p><p>The Mother's Room facility is available at the Indiabulls, Athena, NKP, Wework and at Ecoworld. The room is designed to afford a private lactation space for nursing mothers and resting space for expectant mothers. </p>",
     thumbs: [
       "img/wellness/9930608-wellrec-ss-1440x960-1.jpg",
